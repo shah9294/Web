1af387d57fe15c68001f79feb8c007eb11462be4
shah9294 <shahhussain2044@gmail.com>
2020-08-22 01:16:47 +0500

    populating DB tables

--- Django_blog_app/blog/views.py
+++ Django_blog_app/blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
 
-posts = [
-    {
-        'author':'shah',
-        'title':'Blog Post 1',
-        'content':"First Post Comment",
-        'date_posted':'August 27 2018'
-    },
-    {
-        'author':'hamza',
-        'title':'Blog Post 2',
-        'content':"Second Post Comment",
-        'date_posted':'Sept 27 2018'
-    }
-]
+
 
 
 def home(request):
     context={
-        "posts":posts
+        "posts":Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
